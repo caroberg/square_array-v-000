19b5398d4b0e626075d8cd1e611ad4e0bf0e47e5
Caroline Berg <carolineeberg@gmail.com>
2018-08-03 15:39:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
+  array_new = []
   array.each do |number|
-  new_number = number.to_i**2
-  new_array = [new_number]
-  return new_array
+    new_number = number.to_i**2
+    array_new.push(new_number)
   end
+  return array_new
 end
